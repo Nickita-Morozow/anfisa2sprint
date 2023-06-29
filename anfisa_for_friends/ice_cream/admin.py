eb5ecd110847b7dd7a9e2ba875f8294b3b735fcd
Никита Морозов <morozow.nickita@mail.ru>
2023-06-29 16:52:49 +0300

    Делал на работе

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+from .models import Topping
+from .models import Wrapper
+from .models import IceCream
+
+admin.site.register(Category)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )    
+    search_fields = ('title',) 
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    empty_value_display = 'Не задано'
+    filter_horizontal = ('toppings',)
+
+
+admin.site.register(IceCream, IceCreamAdmin)
